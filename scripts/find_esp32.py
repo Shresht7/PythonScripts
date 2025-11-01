94b93586c2ce98a9a23b9ee6a4ef025bca5e697f
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-11-01 11:31:28 +0530

    Create `find_esp32` script to try and find the COM port that a ESP32 device is connected to

---
+++ scripts/find_esp32.py
@@ -0,0 +1,16 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "pyserial"
+# ]
+# ///
+
+# Library
+from serial.tools.list_ports import comports
+
+for port in comports():
+    # ? Maybe show other information from port
+    print(f"{port.device} - {port.description}", end="")
+    if "CP210" in port.description or "CH340" in port.description:
+        print("    🌟 Likely ESP32!")
+    print()
