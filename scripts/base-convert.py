859a36abebc829deb5ef5adc8e952453eeef2ec2
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-07-01 22:27:57 +0530

    Add base64 encoding and decoding script with CLI interface

---
+++ scripts/base-convert.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env -S uv run
+
+"""
+Encode and decode data with Base64
+"""
+
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#  "defcmd @ git+https://github.com/Shresht7/defcmd.git@v0.5.1"
+# ]
+# ///
+
+import base64
+from defcmd import CLI
+
+
+cli = CLI(description=__doc__)
+
+@cli.subcmd(prompt_optional=False)
+def encode(data: str, url_safe: bool = False):
+    """Encode data in Base64"""
+    bytes = data.encode('utf-8')
+    encoder = base64.urlsafe_b64encode if url_safe else base64.b64encode
+    encoded_data = encoder(bytes).decode('utf-8')
+    print(encoded_data)
+
+@cli.subcmd(prompt_optional=False)
+def decode(data: str, url_safe: bool = False):
+    """Decode Base64 data"""
+    bytes = data.encode('utf-8')
+    decoder = base64.urlsafe_b64decode if url_safe else base64.b64decode
+    decoded_data = decoder(bytes).decode('utf-8')
+    print(decoded_data)
+
+
+if __name__ == "__main__":
+    try:
+        cli.run()
+    except Exception as e:
+        print(f"Error: {e}")
+        exit(1)
